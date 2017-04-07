f0a2b8b33507005224defb216e5276bac5bf50fb
Phillip Creglow <phillipcreglow@msn.com>
2017-04-06 20:30:36 -0400

    Created captions array and event listener to get currentTime of video and add caption-highlight class based on selected span element, tested two captions and passed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 var videoCaptions = document.getElementById('treehouseVideo');
 var captions = [
-  ['caption1', 0.240, 4.130],
-  ['caption2', 4.130, 7.535]
+    ['caption1', 0.240, 4.130],
+    ['caption2', 4.130, 7.535]
 ];
+
+videoCaptions.addEventListener('timeupdate', function() {
+
+    var captionTime = videoCaptions.currentTime;
+
+    captions.forEach(function(caption) {
+        var captionID = caption[0];
+        var captionText = document.getElementById(captionID);
+
+        if (captionTime >= caption[1] && captionTime < caption[2]) {
+            captionText.className = "caption-highlight";
+        } else {
+            captionText.className = '';
+        }
+    });
+});
